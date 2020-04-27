6bc342a595cf958862498f6b031cfcbe5e2ebf6f
Jeffry Gonzalez <enitharmonjmg@outlook.com>
2020-04-27 15:47:58 -0600

    done with day 1

---
+++ LibraryApi/Controllers/StatusController.cs
@@ -0,0 +1,34 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Controllers
+{
+    public class StatusController : Controller
+    {
+        // GET /status -> 200 Ok
+        
+        [HttpGet("/status")]
+        public ActionResult GetTheStatus()
+        {
+            var response = new GetStatusResponse
+            {
+                Message = "Everything is golden!",
+                CheckedBy = "Joe Schmidt",
+                WhenLastChecked = DateTime.Now
+            };
+            return Ok(response);
+        }
+
+    }
+
+    public class GetStatusResponse
+    {
+        public string Message { get;  set; }
+        public string CheckedBy { get; set; }
+        public DateTime WhenLastChecked { get; set; }
+    }
+}
