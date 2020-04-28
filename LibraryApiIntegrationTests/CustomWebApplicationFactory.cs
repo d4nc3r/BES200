92758cd91e7c0657d5e04ee7811d96fca05d3cd3
Jeffry Gonzalez <enitharmonjmg@outlook.com>
2020-04-28 09:43:11 -0600

    before testing

---
+++ LibraryApiIntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,57 @@
+﻿using LibraryApi;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace LibraryApiIntegrationTests
+{
+    public class WebTestFixture : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseEnvironment("Testing");
+
+            builder.ConfigureServices(services =>
+            {
+               
+                
+                var provider = services                   
+                    .BuildServiceProvider();
+
+               
+               
+
+              
+                var sp = services.BuildServiceProvider();
+
+               
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                   
+                    var loggerFactory = scopedServices.GetRequiredService<ILoggerFactory>();
+
+                    var logger = scopedServices
+                        .GetRequiredService<ILogger<WebTestFixture>>();
+
+                 
+                   
+
+                    try
+                    {
+      
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogError(ex, $"An error occurred seeding the " +
+                            "database with test messages. Error: {ex.Message}");
+                    }
+                }
+            });
+        }
+    }
+}
